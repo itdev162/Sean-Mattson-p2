e4a46deb8725c27510557006db5b37960383101e
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-12-13 14:13:30 -0600

    Add project files.

---
+++ DotnetFiveWebApp/Controllers/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetFiveWebApp.Controllers.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
